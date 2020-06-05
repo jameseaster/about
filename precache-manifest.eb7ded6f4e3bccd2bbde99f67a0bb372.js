2b806a616e28a3d9fd02f106fa8ca8c6313faf27
James Easter <james.m.easter@gmail.com>
2020-06-05 18:45:24 -0500

    Updates

---
+++ precache-manifest.eb7ded6f4e3bccd2bbde99f67a0bb372.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6de583619e7fdc87f29849fb2c6aaaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecff1c3a3e6484109395",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb829986b513dc5aa57a",
+    "url": "/static/js/2.f8d4daf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8d4daf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecff1c3a3e6484109395",
+    "url": "/static/js/main.5be67885.chunk.js"
+  },
+  {
+    "revision": "e6db9d95dc0c56975e4b",
+    "url": "/static/js/runtime-main.60e11d02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
