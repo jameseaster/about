0276f05efdb8f3cbf6b55629705dcb6bb17faf20
James Easter <james.m.easter@gmail.com>
2020-06-06 17:37:51 -0500

    Updates

---
+++ precache-manifest.bd7faecf194aa3ed6ebc14baa37eacef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010c44fddaa9be0844ea9b2039163a9d",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "0e76b22b98c9e1297f01",
+    "url": "/about/static/js/2.1d3bbf1f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/about/static/js/2.1d3bbf1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d7c3580792a9d0e4b07",
+    "url": "/about/static/js/main.a4ff3570.chunk.js"
+  },
+  {
+    "revision": "bf01e481d54f70cc4f78",
+    "url": "/about/static/js/runtime-main.5937b91c.js"
+  },
+  {
+    "revision": "0c33df503e2375754c7edcdda95fb08e",
+    "url": "/about/static/media/codewars.0c33df50.png"
+  },
+  {
+    "revision": "e82e21e54417d2d06ea24d3ea270694f",
+    "url": "/about/static/media/dev.e82e21e5.png"
+  },
+  {
+    "revision": "d2edaeac65b1d8660fbd223ea19ad656",
+    "url": "/about/static/media/github.d2edaeac.png"
+  },
+  {
+    "revision": "1c72a7b09dc42dbbe74082233dfba54f",
+    "url": "/about/static/media/linkedin.1c72a7b0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/about/precache-manifest.c23708748ac2bde5e463821b54fba9cf.js"
+  "/about/precache-manifest.bd7faecf194aa3ed6ebc14baa37eacef.js"
 );
 
 self.addEventListener('message', (event) => {
