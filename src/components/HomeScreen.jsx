a96ff19edbe4c41e46def740fbe2e73bbacb67c2
James Easter <james.m.easter@gmail.com>
2020-06-07 16:00:46 -0500

    (feat) complete rough draft of home screen

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import background from '../images/Home_Screen_Background.jpeg';
 
 const HomeScreenStyle = styled.div`
   display: flex;
@@ -11,13 +11,30 @@ const HomeScreenStyle = styled.div`
   background-color: #282c34;
   height: 100vh;
   color: #fff;
+}
+
+.text-container {
+  text-align: left;
+  position: absolute;
+  top: 200px;
+  right: 350px;
+  background-color: rgba(0, 0, 0, 0.3);
+  padding: 50px;
+  border-radius: 10px;
+}
+.background {
+  width: 100%;
+}
 `
 
 const HomeScreen = () => {
   return (
     <HomeScreenStyle id="home">
-      <div>
-        <h3>James Easter</h3>
+      <img src={background} className="background" alt='background'/>
+      <div className='text-container'>
+        <h1>James Easter</h1>
+        <h2>software engineer</h2>
+        <p>I'm a creative, adaptable workhorse</p>
       </div>
     </HomeScreenStyle>
   )
