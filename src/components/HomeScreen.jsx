732e51739c86d3c806f0e81aad58bf21e86f7156
James Easter <james.m.easter@gmail.com>
2020-06-07 16:15:48 -0500

    (fix) remove absolute positioning on homescreen

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -8,29 +8,23 @@ const HomeScreenStyle = styled.div`
   align-items: center;
   justify-content: center;
   text-align: center;
-  background-color: #282c34;
+  background-image: url(${background});
+  background-size: 100%, 100%;
   height: 100vh;
   color: #fff;
 }
 
 .text-container {
   text-align: left;
-  position: absolute;
-  top: 200px;
-  right: 350px;
   background-color: rgba(0, 0, 0, 0.3);
   padding: 50px;
   border-radius: 10px;
-}
-.background {
-  width: 100%;
 }
 `
 
 const HomeScreen = () => {
   return (
     <HomeScreenStyle id="home">
-      <img src={background} className="background" alt='background'/>
       <div className='text-container'>
         <h1>James Easter</h1>
         <h2>software engineer</h2>
