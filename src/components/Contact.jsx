1c50a90f288fef48323554f1529e43646758a866
James Easter <james.m.easter@gmail.com>
2020-06-08 15:59:15 -0500

    (feat) add data to about contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,6 +19,15 @@ const ContactStyle = styled.div`
   background-size: cover;
   height: 100vh;
 }
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    width: 100%;
+    background-color: rgba(0,0,0,0.4);
+  }
 
   .header {
     margin-top: 50px;
@@ -34,15 +43,17 @@ const ContactStyle = styled.div`
   .app-text {
     flex: 1;
     flex-direction: column;
-    text-align: left;
+    text-align: center;
     align-items: center;
     justify-content: center;
     margin: 10%;
     background-color: rgba(0,0,0,0.4);
     padding: 20px;
     border-radius: 8px;
+    border: 1px solid #fff;
+
     @media (min-width: 969px) {
-      width: 50%;
+      width: 100%;
       margin: 5%;
     }
   }
@@ -50,7 +61,7 @@ const ContactStyle = styled.div`
     flex-grow: 3;
   }
   .footer {
-    flex: 1;
+    flex: 0.5;
     display: flex;
     flex-direction: column;
     width: 100%;
@@ -62,25 +73,25 @@ const ContactStyle = styled.div`
 `
 
 const Contact = () => {
+
   return (
-    <ContactStyle id="contact" >
+    <ContactStyle id='contact' >
+      <div className='container'>
       <h3 className='header'>Contact</h3>
       <div className="body-container">
         <div className='app-text'>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Fusce odio quam, pellentesque ac finibus nec, auctor dignissim
-            diam. Nam placerat porta efficitur. Quisque blandit erat non
-            ultrices lacinia.
+          <p style={{marginBottom: '20px'}}>
+            I would love to hear from you,
+            send me an email & stay in touch!
           </p>
-          <h4>Email address</h4>
+          <h4>james.m.easter@gmail.com</h4>
         </div>
       </div>
       <div className='extra-space'></div>
       <div className='footer'>
         <p>built & designed by James Easter</p>
-        <p>add in all of the icons here in white or black</p>
       </div>
+    </div>
     </ContactStyle>
   )
 }
