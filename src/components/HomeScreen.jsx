a2ed0c885d33c281d660fc2675b45dad2ee4669e
James Easter <james.m.easter@gmail.com>
2020-06-08 11:58:26 -0500

    (fix) add mobile responsive design to homescreen

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import background from '../images/Home_Screen_Background.jpeg';
+import background_mobile from '../images/Mobile_Home_Screen_Background.jpeg';
 
 const HomeScreenStyle = styled.div`
   display: flex;
@@ -8,17 +9,34 @@ const HomeScreenStyle = styled.div`
   align-items: center;
   justify-content: center;
   text-align: center;
-  background-image: url(${background});
-  background-size: 100%, 100%;
-  height: 100vh;
+  background: url(${background}) no-repeat center center fixed;
+  background-size: cover;
+  min-height: 100vh;
   color: #fff;
-}
 
 .text-container {
   text-align: left;
   background-color: rgba(0, 0, 0, 0.3);
   padding: 50px;
   border-radius: 10px;
+  margin-left: 250px;
+}
+
+@media (max-width: 768px) {
+  justify-content: flex-start;
+  background: url(${background_mobile}) no-repeat;
+  background-size: cover;
+
+  .text-container {
+    text-align: left;
+    background-color: rgba(0, 0, 0, 0.3);
+    padding: 20px;
+    margin-top: 100px;
+    border-radius: 0px;
+    margin-left: 0px;
+    width: 100vw;
+    height: auto;
+  }
 }
 `
 
