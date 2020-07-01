dceb209caef5d7f469230b06ec6b554bb8ef6cd8
James Easter <james.m.easter@gmail.com>
2020-07-01 18:20:26 -0500

    (feat) add algo-visualizer to projects

--- src/components/Applications.jsx
+++ src/components/Applications.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import flourish from '../images/flourish/flourish_1.PNG';
-import doodle from '../images/doodle/doodle_1.png';
-import howdy from '../images/howdy/howdy_1.png';
+import flourish from "../images/flourish/flourish_1.PNG";
+import doodle from "../images/doodle/doodle_1.png";
+import howdy from "../images/howdy/howdy_1.png";
+import algo from "../images/Algo-Visualizer.png";
 
 const ApplicationsStyle = styled.div`
   display: flex;
@@ -12,9 +13,7 @@ const ApplicationsStyle = styled.div`
   justify-content: center;
   text-align: left;
   background-color: #404040;
-  @media screen
-    and (min-device-width: 1200px)
-    and (max-device-width: 1600px) {
+  @media screen and (min-device-width: 1200px) and (max-device-width: 1600px) {
     min-height: 100vh;
   }
   color: #fff;
@@ -91,80 +90,133 @@ const ApplicationsStyle = styled.div`
   .app-img:hover {
     filter: brightness(50%);
   }
-`
+`;
 
 const Applications = () => {
   return (
-    <ApplicationsStyle id="applications" >
-      <h3 className='header'>Applications</h3>
+    <ApplicationsStyle id="applications">
+      <h3 className="header">Applications</h3>
       <div className="app-container">
-        <div className='app-img'>
-          <a href="https://youtu.be/K9k9t54Spxc" target="_blank" rel="noopener noreferrer">
-            <img src={flourish} className='img flourish' alt='flourish'></img>
+        <div className="app-img">
+          <a
+            href="https://youtu.be/K9k9t54Spxc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={flourish} className="img flourish" alt="flourish"></img>
           </a>
         </div>
-        <div className='app-text'>
-          <h3 style={{margin: '5px'}}>Flourish</h3>
+        <div className="app-text">
+          <h3 style={{ margin: "5px" }}>Flourish</h3>
           <h4>A mobile application for the plant-owning community.</h4>
-          <p style={{margin: '20px 5px 0px 5px'}}>
+          <p style={{ margin: "20px 5px 0px 5px" }}>
             Features: Plant Identification, Augmented Reality, Direct Messaging,
             Posting, Following, & Sharing
           </p>
-          <p style={{margin: '20px 5px 0px 5px'}}>
+          <p style={{ margin: "20px 5px 0px 5px" }}>
             Tech Stack: React Native, Redux, Expo, ngrok, Google Login, PlantId,
             FeatherJS, NodeJS, MariaDB, Three.js
           </p>
-          <a href="https://github.com/Team-GreenDev/flourish" target="_blank" rel="noopener noreferrer">
-            <p style={{margin: '20px 5px 0px 5px'}}>code on github</p>
+          <a
+            href="https://github.com/Team-GreenDev/flourish"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p style={{ margin: "20px 5px 0px 5px" }}>code on github</p>
           </a>
         </div>
       </div>
       <div className="app-container">
-        <div className='app-img'>
-          <a href="https://youtu.be/RrTmyRj_7aw" target="_blank" rel="noopener noreferrer">
-            <img src={doodle} className='img' alt='Doodle Society'></img>
+        <div className="app-img">
+          <a
+            href="https://jameseaster.github.io/algo-visualization/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={algo} className="img" alt="Algo-Visualizer"></img>
           </a>
         </div>
-        <div className='app-text'>
-          <h3 style={{margin: '5px'}}>Doodle Society</h3>
+        <div className="app-text">
+          <h3 style={{ margin: "5px" }}>Algo-Visualizer</h3>
+          <h4>A web application visualize sorting algorithms</h4>
+          <p style={{ margin: "20px 5px 0px 5px" }}>
+            Features: Depicts five different sorting algorithms, visualizing
+            their efficiency and execution
+          </p>
+          <p style={{ margin: "20px 5px 0px 5px" }}>Tech Stack: Vue.js</p>
+          <a
+            href="https://github.com/jameseaster/algo-visualization"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p style={{ margin: "20px 5px 0px 5px" }}>code on github</p>
+          </a>
+        </div>
+      </div>
+      <div className="app-container">
+        <div className="app-img">
+          <a
+            href="https://youtu.be/RrTmyRj_7aw"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={doodle} className="img" alt="Doodle Society"></img>
+          </a>
+        </div>
+        <div className="app-text">
+          <h3 style={{ margin: "5px" }}>Doodle Society</h3>
           <h4>A web application to share doodles.</h4>
-          <p style={{margin: '20px 5px 0px 5px'}}>
-            Features: Geolocation, Live Doodling, Instant Messaging,
-            Sharing via Text Message, & Rankings
+          <p style={{ margin: "20px 5px 0px 5px" }}>
+            Features: Geolocation, Live Doodling, Instant Messaging, Sharing via
+            Text Message, & Rankings
           </p>
-          <p style={{margin: '20px 5px 0px 5px'}}>
-            Tech Stack: React, Twilio, Bootstrap, Pusher, Cloudinary,
-            Fastify, NodeJS, Postgres, Google Maps
+          <p style={{ margin: "20px 5px 0px 5px" }}>
+            Tech Stack: React, Twilio, Bootstrap, Pusher, Cloudinary, Fastify,
+            NodeJS, Postgres, Google Maps
           </p>
-          <a href="https://github.com/team-tauros/doodle-society-1" target="_blank" rel="noopener noreferrer">
-            <p style={{margin: '20px 5px 0px 5px'}}>code on github</p>
+          <a
+            href="https://github.com/team-tauros/doodle-society-1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p style={{ margin: "20px 5px 0px 5px" }}>code on github</p>
           </a>
         </div>
       </div>
       <div className="app-container">
-        <div className='app-img'>
-          <a href="https://youtu.be/wAt3b_yNUwU" target="_blank" rel="noopener noreferrer">
-            <img src={howdy} className='img' alt='Howdy'></img>
+        <div className="app-img">
+          <a
+            href="https://youtu.be/wAt3b_yNUwU"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={howdy} className="img" alt="Howdy"></img>
           </a>
         </div>
-        <div className='app-text'>
-          <h3 style={{margin: '5px'}}>Howdy</h3>
-          <h4>A web application for throwing virtual parties with your neighbors</h4>
-          <p style={{margin: '20px 5px 0px 5px'}}>
-            Features: Geolocation, Google Authentication, Live Messaging,
-            Game ("Who Am I?"), & Creating a Party
+        <div className="app-text">
+          <h3 style={{ margin: "5px" }}>Howdy</h3>
+          <h4>
+            A web application for throwing virtual parties with your neighbors
+          </h4>
+          <p style={{ margin: "20px 5px 0px 5px" }}>
+            Features: Geolocation, Google Authentication, Live Messaging, Game
+            ("Who Am I?"), & Creating a Party
           </p>
-          <p style={{margin: '20px 5px 0px 5px'}}>
+          <p style={{ margin: "20px 5px 0px 5px" }}>
             Tech Stack: React, Socket.io, React-Router, Google Auth, Axios,
             Express, NodeJS, MySql
           </p>
-          <a href="https://github.com/team-tauros/Howdy" target="_blank" rel="noopener noreferrer">
-            <p style={{margin: '20px 5px 0px 5px'}}>code on github</p>
+          <a
+            href="https://github.com/team-tauros/Howdy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p style={{ margin: "20px 5px 0px 5px" }}>code on github</p>
           </a>
         </div>
       </div>
     </ApplicationsStyle>
-  )
-}
+  );
+};
 
-export default Applications
+export default Applications;
