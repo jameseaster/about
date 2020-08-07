2929317252331bf2530b0abdd24c30bed5dd8669
James Easter <james.m.easter@gmail.com>
2020-08-07 08:32:04 -0500

    Updates

---
+++ precache-manifest.e0f1bcf5e23ac9c6797baf20181f4fbe.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6966616decdbf6f28a844559c1cf76e",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "a0fe1cbe5d627a2ab673",
+    "url": "/about/static/js/2.f77012f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/about/static/js/2.f77012f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da21c98c05e77995d3b3",
+    "url": "/about/static/js/main.ff000fcf.chunk.js"
+  },
+  {
+    "revision": "77854eb316008fc648f3",
+    "url": "/about/static/js/runtime-main.c7fcc918.js"
+  },
+  {
+    "revision": "ed44d49485a09b6b1ed583617a887008",
+    "url": "/about/static/media/Home_Screen_Background.ed44d494.jpeg"
+  },
+  {
+    "revision": "b5d28b7ecacccffb45c29e0a34dcfaec",
+    "url": "/about/static/media/JS_HTML_CSS.b5d28b7e.png"
+  },
+  {
+    "revision": "49830250d6808cba3c041da17a08ece1",
+    "url": "/about/static/media/Mobile_Home_Screen_Background.49830250.jpeg"
+  },
+  {
+    "revision": "68b8f54145160b776ff82b950381500a",
+    "url": "/about/static/media/Resume.68b8f541.pdf"
+  },
+  {
+    "revision": "0c33df503e2375754c7edcdda95fb08e",
+    "url": "/about/static/media/codewars.0c33df50.png"
+  },
+  {
+    "revision": "e82e21e54417d2d06ea24d3ea270694f",
+    "url": "/about/static/media/dev.e82e21e5.png"
+  },
+  {
+    "revision": "f1e23f3d2cbf70c2ba864c91947df00d",
+    "url": "/about/static/media/doodle_1.f1e23f3d.png"
+  },
+  {
+    "revision": "c43851b71505a51cb58e67d1202f5d9e",
+    "url": "/about/static/media/expo.c43851b7.png"
+  },
+  {
+    "revision": "ab2d5e0dfef71cfec96b08ec53178a6e",
+    "url": "/about/static/media/feathersjs.ab2d5e0d.png"
+  },
+  {
+    "revision": "fb944534b821d2da2131ed5b17104dcd",
+    "url": "/about/static/media/flourish_1.fb944534.PNG"
+  },
+  {
+    "revision": "d2edaeac65b1d8660fbd223ea19ad656",
+    "url": "/about/static/media/github.d2edaeac.png"
+  },
+  {
+    "revision": "5a427f86e0940a33a792a77eb392d865",
+    "url": "/about/static/media/howdy_1.5a427f86.png"
+  },
+  {
+    "revision": "1c72a7b09dc42dbbe74082233dfba54f",
+    "url": "/about/static/media/linkedin.1c72a7b0.png"
+  },
+  {
+    "revision": "ee88752d573140a8f6e0f476114560ee",
+    "url": "/about/static/media/nodejs.ee88752d.png"
+  },
+  {
+    "revision": "dfe0d34b0936833629ec7c7b65bbf1be",
+    "url": "/about/static/media/react.dfe0d34b.png"
+  },
+  {
+    "revision": "9fe90d7c8ea9b3dbb4b28c2487910be0",
+    "url": "/about/static/media/sorting-visualizer.9fe90d7c.png"
+  }
+]);
